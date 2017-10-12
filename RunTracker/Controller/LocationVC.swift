8da0144ebf9de7442fa342db0917b0d3cc15358d
Miguel Santos <miggsantos@gmail.com>
2017-10-12 22:05:06 +0100

    mpaview a LocationVC finished

---
+++ RunTracker/Controller/LocationVC.swift
@@ -0,0 +1,35 @@
+//
+//  LocationVC.swift
+//  RunTracker
+//
+//  Created by Miguel Santos on 12/10/2017.
+//  Copyright © 2017 Miguel Santos. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LocationVC: UIViewController, MKMapViewDelegate {
+
+    var manager: CLLocationManager?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        manager = CLLocationManager()
+        manager?.desiredAccuracy = kCLLocationAccuracyBest
+        manager?.activityType = .fitness
+        
+
+    }
+    
+    func checkLocationAuthStatus() {
+        if CLLocationManager.authorizationStatus() != .authorizedWhenInUse {
+            manager?.requestWhenInUseAuthorization()
+            
+        }
+    }
+
+
+
+}
