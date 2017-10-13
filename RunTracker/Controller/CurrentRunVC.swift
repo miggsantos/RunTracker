3d8cf7b21fa864bf27057cfe660544be4bfa315e
Miguel Santos <miggsantos@gmail.com>
2017-10-13 01:48:57 +0100

    Runner location updating to label

--- RunTracker/Controller/CurrentRunVC.swift
+++ RunTracker/Controller/CurrentRunVC.swift
@@ -7,14 +7,21 @@
 //
 
 import UIKit
+import MapKit
 
 class CurrentRunVC: LocationVC {
 
     
     @IBOutlet weak var swipeBGImageView: UIImageView!
     @IBOutlet weak var sliderImageView: UIImageView!
+    @IBOutlet weak var durationLbl: UILabel!
+    @IBOutlet weak var paceLbl: UILabel!
+    @IBOutlet weak var distanceLbl: UILabel!
+    @IBOutlet weak var pauseBtn: UIButton!
     
-    
+    var startLocation: CLLocation!
+    var lastLocation: CLLocation!
+    var runDistance = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,8 +31,26 @@ class CurrentRunVC: LocationVC {
         sliderImageView.isUserInteractionEnabled = true
         swipeGesture.delegate = self as? UIGestureRecognizerDelegate
     }
+    
 
+    override func viewWillAppear(_ animated: Bool) {
+        manager?.delegate = self
+        manager?.distanceFilter = 10
+        startRun()
+    }
 
+    func startRun(){
+        manager?.startUpdatingLocation()
+    }
+    
+    func endRun(){
+        manager?.stopUpdatingLocation()
+    }
+
+    @IBAction func pauseButtonPressed(_ sender: Any) {
+    
+    }
+    
     @objc func endRunSwiped(sender: UIPanGestureRecognizer) {
         let minAdjust: CGFloat = 80
         let maxAdjust: CGFloat = 128
@@ -51,4 +76,23 @@ class CurrentRunVC: LocationVC {
         }
     }
 
+}
+
+extension CurrentRunVC: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            checkLocationAuthStatus()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if startLocation == nil {
+            startLocation = locations.first
+        } else if let location = locations.last {
+            runDistance += lastLocation.distance(from: location)
+            distanceLbl.text = "\(runDistance)"
+        }
+        lastLocation = locations.last
+    }
 }
