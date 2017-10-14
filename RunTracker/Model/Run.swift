75a1075d0bbe85d106b75eeab2d8917a5bc7bfa0
Miguel Santos <miggsantos@gmail.com>
2017-10-14 15:14:08 +0100

    created realm obj

---
+++ RunTracker/Model/Run.swift
@@ -0,0 +1,36 @@
+//
+//  Run.swift
+//  RunTracker
+//
+//  Created by Miguel Santos on 14/10/2017.
+//  Copyright © 2017 Miguel Santos. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Run: Object {
+    @objc dynamic public private(set) var id = ""
+    @objc dynamic public private(set) var date = NSDate()
+    @objc dynamic public private(set) var pace = 0
+    @objc dynamic public private(set) var distance = 0.0
+    @objc dynamic public private(set) var duration = 0
+    
+    override class func primaryKey() -> String {
+        return "id"
+    }
+    
+    override class func indexedProperties() -> [String] {
+        return ["pace", "date", "duration"]
+    }
+    
+    convenience init(pace: Int, distance: Double, duration: Int) {
+        self.init()
+        self.id = UUID().uuidString.lowercased()
+        self.date = NSDate()
+        self.pace = pace
+        self.distance = distance
+        self.duration = duration
+    }
+    
+}
